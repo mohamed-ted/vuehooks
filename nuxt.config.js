2d70203a2026b0ba8483d733128916b691597e7b
Mohamed Meftah <mohamed-ted@live.com>
2020-11-04 10:29:03 +0100

    fix github pages deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,7 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'vuehooks',
+    title: 'Vue Hooks',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -13,29 +10,21 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/google-fonts',
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
   googleFonts: {
     families: {
@@ -43,6 +32,6 @@ export default {
     },
     display: 'swap',
   },
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
+
   build: {},
 }
